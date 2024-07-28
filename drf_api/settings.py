59f935fc2ea9e0b67ce76286ce1f3e200ae7a98b
Mohamed Abdillahi <mohamedabdillahi2@hotmail.com>
2024-07-28 19:42:38 +0000

    added profile detail view

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -21,9 +21,9 @@ CLOUDINARY_STORAGE = {
 }
 MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -40,6 +40,10 @@ ALLOWED_HOSTS = [
     '8000-mabdillahi88-drfapi-272ty7zpoxe.ws.codeinstitute-ide.net',
 ]
 
+# Add CSRF trusted origins
+CSRF_TRUSTED_ORIGINS = [
+    'https://8000-mabdillahi88-drfapi-272ty7zpoxe.ws.codeinstitute-ide.net',
+]
 
 # Application definition
 
@@ -53,7 +57,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cloudinary',
     'rest_framework',
-
     'profiles',
 ]
 
@@ -87,7 +90,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'drf_api.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -97,7 +99,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -117,7 +118,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -130,7 +130,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
