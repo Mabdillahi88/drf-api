67cc893029dfa3ae149cddec62c4a33b70f72c3f
Mohamed Abdillahi <mohamedabdillahi2@hotmail.com>
2024-07-31 12:04:57 +0000

    created like list and like detail

--- likes/views.py
+++ likes/views.py
@@ -1,20 +1,25 @@
-from rest_framework import generics, permissions, status
-from rest_framework.response import Response
-from rest_framework.exceptions import ValidationError
-from .models import Like
-from .serializers import LikeSerializer
+from rest_framework import generics, permissions
+from drf_api.permissions import IsOwnerOrReadOnly
+from likes.models import Like
+from likes.serializers import LikeSerializer
+
 
 class LikeList(generics.ListCreateAPIView):
-    queryset = Like.objects.all()
-    serializer_class = LikeSerializer
+    """
+    List likes or create a like if logged in.
+    """
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = LikeSerializer
+    queryset = Like.objects.all()
 
     def perform_create(self, serializer):
-        if Like.objects.filter(owner=self.request.user, post=serializer.validated_data['post']).exists():
-            raise ValidationError({'detail': 'You have already liked this post.'})
         serializer.save(owner=self.request.user)
 
+
 class LikeDetail(generics.RetrieveDestroyAPIView):
-    queryset = Like.objects.all()
+    """
+    Retrieve a like or delete it by id if you own it.
+    """
+    permission_classes = [IsOwnerOrReadOnly]
     serializer_class = LikeSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Like.objects.all()
