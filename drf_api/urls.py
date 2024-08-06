2ccc08e3e7080b1525c00b1df20d3214cb715b75
Mohamed Abdillahi <mohamedabdillahi2@hotmail.com>
2024-08-06 22:10:59 +0000

    changes to views

--- drf_api/urls.py
+++ drf_api/urls.py
@@ -1,27 +1,12 @@
-"""drf_api URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import RedirectView
-from .views import root_route
+from .views import root_route, logout_route
 
 urlpatterns = [
     path('', root_route),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
+    path('dj-rest-auth/logout/', logout_route),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
     path('profiles/', include('profiles.urls')),
@@ -29,5 +14,4 @@ urlpatterns = [
     path('comments/', include('comments.urls')),
     path('likes/', include('likes.urls')),
     path('followers/', include('followers.urls')),
-    path('', RedirectView.as_view(url='/posts/')),
 ]
